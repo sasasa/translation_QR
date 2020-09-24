8d6a9300ac729b6397f57a05fd009f2298a20319
sasasa <masaakisaeki@gmail.com>
2020-09-24 16:45:39 +0900

    日、週、月などで売り上げ合計を見ることが出来る #43
    まだ不備があるがテストデータ作成スクリプト

--- database/seeds/OrdersTableSeeder.php
+++ database/seeds/OrdersTableSeeder.php
@@ -31,5 +31,56 @@ class OrdersTableSeeder extends Seeder
         //     'created_at' => now(),
         //     'updated_at' => now(),
         // ]);
+
+        $items = \App\Item::get();
+        \App\SeatSession::all()->each(function($seatSession) use($items){
+            $order_num = mt_rand(2, 12);
+            $is_take_out = mt_rand(0, 1);
+            $items->random($order_num)->each(function($item) use($seatSession, $is_take_out){
+
+                if ($is_take_out) {
+                    DB::table('orders')->insert([
+                        'seat_session_id' => $seatSession->id,
+                        'item_id' => $item->id,
+                        'order_state' => 'preparation',
+                        'order_price' => $item->item_price,
+                        // テイクアウトかどうか
+                        'is_take_out' => true,
+                        // 消費税率
+                        'tax_rate' => 0.08,
+                        // 消費税額
+                        'sales_tax' => ceil(bcmul($item->item_price, 0.08, 1)),
+                        // 税込み金額
+                        'tax_included_price' => ceil(bcmul($item->item_price, 1.08, 1)),    
+                        'created_at' => $seatSession->created_at,
+                        'updated_at' => $seatSession->created_at,
+                    ]);
+                } else {
+                    DB::table('orders')->insert([
+                        'seat_session_id' => $seatSession->id,
+                        'item_id' => $item->id,
+                        'order_state' => 'preparation',
+                        'order_price' => $item->item_price,
+                        // テイクアウトかどうか
+                        'is_take_out' => false,
+                        // 消費税率
+                        'tax_rate' => 0.1,
+                        // 消費税額
+                        'sales_tax' => ceil(bcmul($item->item_price, 0.1, 1)),
+                        // 税込み金額
+                        'tax_included_price' => ceil(bcmul($item->item_price, 1.1, 1)),    
+                        'created_at' => $seatSession->created_at,
+                        'updated_at' => $seatSession->created_at,
+                    ]);
+                }
+            });
+            $sum = $seatSession->orders->map(function($order){
+                return $order->tax_included_price;
+            })->sum();
+            $payment = new \App\Payment();
+            $payment->tax_included_price = $sum;
+            $payment->seat_session_id = $seatSession->id;
+            $payment->save();
+        });
     }
 }
