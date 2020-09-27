c6a33d5e93b46c9f2f481f0103979c9acd21ccaf
sasasa <masaakisaeki@gmail.com>
2020-09-27 14:43:40 +0900

    jsのエラーが消えるように修正

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,3 +14,5 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sass('resources/sass/print.scss', 'public/css/print.css');
+
+mix.sourceMaps().js('node_modules/popper.js/dist/popper.js', 'public/js').sourceMaps();
