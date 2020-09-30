2cecfa60f22216d5e3645bfc44eb954c0f323337
sasasa <masaakisaeki@gmail.com>
2020-09-30 10:10:40 +0900

    日、週、月などで売り上げ合計を見ることが出来る #43
    ボタンの表示を整えた。

--- resources/views/orders/sum_total.blade.php
+++ resources/views/orders/sum_total.blade.php
@@ -15,42 +15,44 @@
   </div>
   <input type="submit" value="検索" class="btn btn-primary">
 </form>
-<form action="/sum_total" method="get" class="mb-2">
-  @csrf
-  <input type="hidden" name="search_start_at" value="{{$yesterday}}">
-  <input type="hidden" name="search_end_at" value="{{$today}}">
-  <input type="submit" value="昨日" class="btn btn-primary">
-</form>
-<form action="/sum_total" method="get" class="mb-2">
-  @csrf
-  <input type="hidden" name="search_start_at" value="{{$today}}">
-  <input type="hidden" name="search_end_at" value="{{$tomorrow}}">
-  <input type="submit" value="今日" class="btn btn-primary">
-</form>
-<form action="/sum_total" method="get" class="mb-2">
-  @csrf
-  <input type="hidden" name="search_start_at" value="{{$lastOfWeek}}">
-  <input type="hidden" name="search_end_at" value="{{$startOfWeek}}">
-  <input type="submit" value="先週" class="btn btn-primary">
-</form>
-<form action="/sum_total" method="get" class="mb-2">
-  @csrf
-  <input type="hidden" name="search_start_at" value="{{$startOfWeek}}">
-  <input type="hidden" name="search_end_at" value="{{$nextOfWeek}}">
-  <input type="submit" value="今週" class="btn btn-primary">
-</form>
-<form action="/sum_total" method="get" class="mb-2">
-  @csrf
-  <input type="hidden" name="search_start_at" value="{{$lastOfMonth}}">
-  <input type="hidden" name="search_end_at" value="{{$startOfMonth}}">
-  <input type="submit" value="先月" class="btn btn-primary">
-</form>
-<form action="/sum_total" method="get" class="mb-5">
-  @csrf
-  <input type="hidden" name="search_start_at" value="{{$startOfMonth}}">
-  <input type="hidden" name="search_end_at" value="{{$nextOfMonth}}">
-  <input type="submit" value="今月" class="btn btn-primary">
-</form>
+<div class="d-flex">
+  <form action="/sum_total" method="get" class="mb-2 mr-2">
+    @csrf
+    <input type="hidden" name="search_start_at" value="{{$yesterday}}">
+    <input type="hidden" name="search_end_at" value="{{$today}}">
+    <input type="submit" value="昨日" class="btn btn-primary">
+  </form>
+  <form action="/sum_total" method="get" class="mb-2 mr-2">
+    @csrf
+    <input type="hidden" name="search_start_at" value="{{$today}}">
+    <input type="hidden" name="search_end_at" value="{{$tomorrow}}">
+    <input type="submit" value="今日" class="btn btn-primary">
+  </form>
+  <form action="/sum_total" method="get" class="mb-2 mr-2">
+    @csrf
+    <input type="hidden" name="search_start_at" value="{{$lastOfWeek}}">
+    <input type="hidden" name="search_end_at" value="{{$startOfWeek}}">
+    <input type="submit" value="先週" class="btn btn-primary">
+  </form>
+  <form action="/sum_total" method="get" class="mb-2 mr-2">
+    @csrf
+    <input type="hidden" name="search_start_at" value="{{$startOfWeek}}">
+    <input type="hidden" name="search_end_at" value="{{$nextOfWeek}}">
+    <input type="submit" value="今週" class="btn btn-primary">
+  </form>
+  <form action="/sum_total" method="get" class="mb-2 mr-2">
+    @csrf
+    <input type="hidden" name="search_start_at" value="{{$lastOfMonth}}">
+    <input type="hidden" name="search_end_at" value="{{$startOfMonth}}">
+    <input type="submit" value="先月" class="btn btn-primary">
+  </form>
+  <form action="/sum_total" method="get" class="mb-5 mr-2">
+    @csrf
+    <input type="hidden" name="search_start_at" value="{{$startOfMonth}}">
+    <input type="hidden" name="search_end_at" value="{{$nextOfMonth}}">
+    <input type="submit" value="今月" class="btn btn-primary">
+  </form>
+</div>
 
 <h2>
   売上げ計(税込み):{{number_format($sum_sales)}}円
