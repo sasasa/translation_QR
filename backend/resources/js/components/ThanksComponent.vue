72f8164261e672a3ff98bf46569aeb52df57463c
masasaeki <masaakisaeki@gmail.com>
2020-10-20 12:19:45 +0900

    SPAで読み込み時にローディングを入れる #95

--- backend/resources/js/components/ThanksComponent.vue
+++ backend/resources/js/components/ThanksComponent.vue
@@ -1,32 +1,41 @@
 <template>
     <div>
-        <h1>{{$t('message.thanks_wait')}}</h1>
-        <div class="h1">{{all_items}}点{{all_price}}円</div>
+        <Loading v-show="loading"></Loading>
         <div>
-            ({{$t('message.tax')}})
-        </div>
+            <h1>{{$t('message.thanks_wait')}}</h1>
+            <div class="h1">{{all_items}}点{{all_price}}円</div>
+            <div>
+                ({{$t('message.tax')}})
+            </div>
 
-        <div v-for="(take_out_orders, is_take_out) in ordered_orders" v-bind:key="is_take_out">
-            <span v-if="is_take_out">
-                <br>{{$t('message.takeout')}}
-            </span>
-            <span v-else>
-                <br>{{$t('message.food_and_drink_in_the_store')}}
-            </span>
-            <ul>
-                <li v-for="(orders, item_name) in take_out_orders" v-bind:key="orders[0].id">
-                    {{item_name}}---
-                    {{orders.length}}<br>
-                    {{orders.reduce((accumulator, order)=>{return order.tax_included_price + accumulator}, 0)}}円
-                </li>
-            </ul>
+            <div v-for="(take_out_orders, is_take_out) in ordered_orders" v-bind:key="is_take_out">
+                <span v-if="is_take_out">
+                    <br>{{$t('message.takeout')}}
+                </span>
+                <span v-else>
+                    <br>{{$t('message.food_and_drink_in_the_store')}}
+                </span>
+                <ul>
+                    <li v-for="(orders, item_name) in take_out_orders" v-bind:key="orders[0].id">
+                        {{item_name}}---
+                        {{orders.length}}<br>
+                        {{orders.reduce((accumulator, order)=>{return order.tax_included_price + accumulator}, 0)}}円
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
+
 </template>
 
 <script>
+    import Loading from './Loading'
+
     export default {
         name: 'thanks-component',
+        components: {
+            Loading,
+        },
         props: {
             session_key: {
                 type: String,
@@ -46,6 +55,7 @@
                 ordered_orders: [],
                 all_items: 0,
                 all_price: 0,
+                loading: true,
             }
         },
         computed: {
@@ -62,6 +72,7 @@
                     this.ordered_orders = response.data.ordered_orders
                     this.all_items = response.data.all_items
                     this.all_price = response.data.all_price
+                    this.loading = false
                     // alert(JSON.stringify(response.data))
                 })
                 .catch((error) => {
@@ -69,7 +80,6 @@
                     console.log(error);
                 })
             this.$i18n.locale = this.lang
-            
             // 戻るボタンを無効化
             history.pushState(null, null, location.href);
             window.addEventListener('popstate', (e) => {
