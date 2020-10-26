ba58d3850acc44ba67a3a264bd82a08c74e45f97
sasasa <masaakisaeki@gmail.com>
2020-10-26 15:49:55 +0900

    テイクアウトと店内飲食を買うとサンクス画面でどちらもテイクアウトと表示されてしまう #106

--- backend/public/js/popper.js
+++ backend/public/js/popper.js
@@ -116,7 +116,7 @@ eval("var g;\n\n// This works in non-strict mode\ng = (function() {\n\treturn th
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /work/node_modules/popper.js/dist/popper.js */"./node_modules/popper.js/dist/popper.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\translation_QR\backend\node_modules\popper.js\dist\popper.js */"./node_modules/popper.js/dist/popper.js");
 
 
 /***/ })
